9243d0779d7e95f329e0509c52029cfbe9136992
supriya chakraborty <Suprchak@outlook.com>
2022-05-06 01:53:16 +0530

    Test

---
+++ leave-management/Data/Migrations/20211212183648_IntialForLaptop.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Data.Migrations
+{
+    public partial class IntialForLaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultDays",
+                table: "DetailsLeaveTypeVM",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeVM",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Firstname = table.Column<string>(nullable: true),
+                    Lastname = table.Column<string>(nullable: true),
+                    TaxId = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    DateJoined = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeVM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EditLeaveAllocationVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<string>(nullable: true),
+                    NumberOfDays = table.Column<int>(nullable: false),
+                    LeaveTypeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EditLeaveAllocationVM", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EditLeaveAllocationVM_EmployeeVM_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "EmployeeVM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EditLeaveAllocationVM_DetailsLeaveTypeVM_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "DetailsLeaveTypeVM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveAllocationVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfDays = table.Column<int>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    Period = table.Column<int>(nullable: false),
+                    EmployeeId = table.Column<string>(nullable: true),
+                    LeaveTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveAllocationVM", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocationVM_EmployeeVM_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "EmployeeVM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocationVM_DetailsLeaveTypeVM_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "DetailsLeaveTypeVM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EditLeaveAllocationVM_EmployeeId",
+                table: "EditLeaveAllocationVM",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EditLeaveAllocationVM_LeaveTypeId",
+                table: "EditLeaveAllocationVM",
+                column: "LeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocationVM_EmployeeId",
+                table: "LeaveAllocationVM",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocationVM_LeaveTypeId",
+                table: "LeaveAllocationVM",
+                column: "LeaveTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EditLeaveAllocationVM");
+
+            migrationBuilder.DropTable(
+                name: "LeaveAllocationVM");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeVM");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultDays",
+                table: "DetailsLeaveTypeVM");
+        }
+    }
+}
